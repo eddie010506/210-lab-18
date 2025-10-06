08fa877791955630ac7734c53156708b59ca646f
Edward Choi <edwardchoi2001@gmail.com>
2025-10-05 20:28:54 -0700

    third

--- main.cpp
+++ main.cpp
@@ -28,6 +28,54 @@ int main() {
     cin >> choice;
     // input verification
     while (cin.fail() || (choice != 1 && choice != 2)){
-        
+        cout << "Invalid input. Please enter 1 or 2: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> choice;    
     }
+    char another = 'y';
+    do {
+        double rating = -1.0;
+        string comment;
+
+        // get the review rating from the user with validation
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        while (cin.fail() || rating < 0 || rating > 5) {
+             cout << "Invalid rating. Please enter a number between 0 and 5: ";
+             cin.clear();
+             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+             cin >> rating;
+        }
+
+        // get the review comment
+        cout << "Enter review comments: ";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the newline character left by cin
+        getline(cin, comment);
+
+        // add the new node based on the user's initial choice
+        if (choice == 1) {
+            addNodeToHead(head, tail, rating, comment);
+        } else { 
+            addNodeToTail(head, tail, rating, comment);
+        }
+
+        // ask if the user wants to enter another review
+        cout << "Enter another review? Y/N: ";
+        cin >> another;
+
+    } while (another == 'y' || another == 'Y');
+
+    
+    printListAndAverage(head);
+
+    
+    cleanup(head);
+
+    return 0;
+}
+
+
+void addNodeToTail(ReviewNode*& head, ReviewNode*& tail, double rating, const string& comment) {
+    
 }
